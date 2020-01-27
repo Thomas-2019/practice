7657b2133d23a99dcb35c4a9820732b066c1e674
Thomas <a_s1014@yahoo.com.tw>
2020-01-27 21:21:32 +0800

    add-vue-day16單檔加入

---
+++ vue/day16/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <TitleComponent :title="title" /><!-- 加入component tag 用v-bind綁定 -->
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TitleComponent from "./components/Title.vue"; //tag標籤 import 進來和入徑
+
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Welcome to Your Vue.js App"//綁定資料 v-bind
+    };
+  },
+  components: {
+    HelloWorld,
+    TitleComponent //加入TitleComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/day16/src/components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- 單一個檔加入樣式 -->
+  <h1 class="title">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "TitleComponent",//命名要使用component
+  // data() {
+  //   return {
+  //     title: "Thomas!Hello World"
+  //   };
+  // },//從內部傳資料:方法一
+  props:{
+    title:{
+      type:String,
+      required:true
+    }
+  }//從app.vue(外部)傳資料這裡接收需使用props:方法二
+};
+</script>
+
+<style scoped>
+h1.title {
+  color: orange;
+}
+</style>
