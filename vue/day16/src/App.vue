73e4b522530c2c7f4f995b26541b9164355b8515
Thomas <a_s1014@yahoo.com.tw>
2020-01-28 14:47:40 +0800

    add-vue-day16分檔模組化

--- vue/day16/src/App.vue
+++ vue/day16/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <TitleComponent :title="title" /><!-- 加入component tag 用v-bind綁定 -->
+    <!--<InputComponent :text="title" @textchange="changHandler"/> 加入components中的InputComponent-index tag 用v-bind綁定 @textchange在methods裡綁函式(方法1)-->
+    <InputComponent :text.sync="title" /><!--加入.sync,vue內建方法(方法2)-->
+    <TitleComponent :title="title" /><!-- 加入components中的Title tag 用v-bind綁定 -->
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,7 +10,8 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import TitleComponent from "./components/Title.vue"; //tag標籤 import 進來和入徑
+import TitleComponent from "./components/Title.vue"; //import Title標籤路徑
+import InputComponent from "./components/InputComponent/index.vue" //import index標籤路徑
 
 export default {
   name: "app",
@@ -19,8 +22,14 @@ export default {
   },
   components: {
     HelloWorld,
-    TitleComponent //加入TitleComponent
-  }
+    TitleComponent, //components加入TitleComponent
+    InputComponent  //components加入InputComponent
+  },
+  //methods:{
+    //changHandler(val){
+      //this.title=val
+   // }//綁定子層(index.vue)與父層(app.vue)函式,將val值帶入this.title(方法1)
+  //}
 };
 </script>
 
---
+++ vue/day16/src/components/InputComponent/index.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name:"Input-Component",
+  props:{
+    text:{
+      type:String,
+      required:true
+    }
+  },
+  computed:{
+    InputText:{
+      get(){
+        return this.text
+      },
+      set(val){
+        // eslint-disable-next-line no-console
+        // console.log(val)
+        // this.$emit('textchange',val)//將val值放入v-on:textchange函式中(方法1)
+        this.$emit('update:text',val)//(方法2)
+      }
+    }//取得InputComponent的input取得輸入值
+  }
+}
+</script>
+
+<template src="./template.html"></template>
+<style scoped src="./style.css"></style>
